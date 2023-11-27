06cdcf7180ea4c011a66b39305747ff90dd1c837
scabbage <thescabbage@gmail.com>
2023-11-27 23:57:50 +0800

    Use frecency for default telescope menu

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tt', builtin.find_files, {})
+
+-- vim.keymap.set('n', '<leader>tt', builtin.find_files, {})
+vim.keymap.set('n', '<leader>tt', '<Cmd>Telescope frecency workspace=CWD<CR>')
 vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+
 vim.keymap.set('n', '<leader>th', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
-vim.keymap.set('n', '<leader>tf', '<Cmd>Telescope frecency workspace=CWD<CR>')
