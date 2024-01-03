26ea2c09d23314ad31c0067cd48a630848442d83
scabbage <thescabbage@gmail.com>
2024-01-04 04:32:53 +0800

    Add shortcuts for global marks

--- .config/nvim/lua/scabbage/remap.lua
+++ .config/nvim/lua/scabbage/remap.lua
@@ -5,3 +5,8 @@ vim.keymap.set("n", "<leader>gpt", vim.cmd.ChatGPT)
 vim.keymap.set("n", "<leader>cmp", vim.cmd.ChatGPTCompleteCode)
 
 vim.keymap.set("n", "<leader>pl", vim.cmd.Gpush)
+
+vim.api.nvim_set_keymap('n', '<C-h>', [[<Cmd>normal! 'H<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-t>', [[<Cmd>normal! 'T<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-n>', [[<Cmd>normal! 'N<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-s>', [[<Cmd>normal! 'S<CR>]], {noremap = true})
