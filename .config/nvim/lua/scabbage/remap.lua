a31fda7e668d00c33db48ff6cdb73e39ed10b26c
scabbage <thescabbage@gmail.com>
2024-01-04 04:42:54 +0800

    Make global marks shortcuts less shit

--- .config/nvim/lua/scabbage/remap.lua
+++ .config/nvim/lua/scabbage/remap.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>cmp", vim.cmd.ChatGPTCompleteCode)
 
 vim.keymap.set("n", "<leader>pl", vim.cmd.Gpush)
 
-vim.api.nvim_set_keymap('n', '<C-h>', [[<Cmd>normal! 'H<CR>]], {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-t>', [[<Cmd>normal! 'T<CR>]], {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-n>', [[<Cmd>normal! 'N<CR>]], {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-s>', [[<Cmd>normal! 'S<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>jh', [[<Cmd>normal! 'H<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>jt', [[<Cmd>normal! 'T<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>jn', [[<Cmd>normal! 'N<CR>]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>js', [[<Cmd>normal! 'S<CR>]], {noremap = true})
