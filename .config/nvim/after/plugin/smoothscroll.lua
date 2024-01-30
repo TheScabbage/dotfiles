edbb52e2aaf3372061a323312c838e99ddef5ca1
scabbage <thescabbage@gmail.com>
2024-01-31 00:20:16 +0800

    Fix smoothscroll derping out when jumping in tag stack

--- .config/nvim/after/plugin/smoothscroll.lua
+++ .config/nvim/after/plugin/smoothscroll.lua
@@ -1,6 +1,6 @@
 require('cinnamon').setup {
     default_delay = 10,
-    always_scroll = true,
+    always_scroll = false,
     scroll_limit  = 300,
     max_length     = 300,
     extra_keymaps = true,
