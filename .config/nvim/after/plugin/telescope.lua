ea26a078d79163dc3d7ef4bcc5537e2c187d48ef
scabbage <thescabbage@gmail.com>
2024-03-05 23:18:58 +0800

    Add bind for telescope help tags

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -34,14 +34,14 @@ telescope.load_extension("ui-select")
 
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tt', '<Cmd>Telescope frecency workspace=CWD<CR>')
--- vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>tr', builtin.registers, {})
 
-vim.keymap.set('n', '<leader>th', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+--vim.keymap.set('n', '<leader>th', function()
+--    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+--end)
